74a83ffa7e8080e1be5537b74f683e8cb412d304
rainman-77 <ashibarcavill99@gmail.com>
2024-10-05 11:10:00 +0530

    added unit test for search functionality

---
+++ tests/test_search.py
@@ -0,0 +1,23 @@
+import pytest
+from pageobjects.home_page import HomePage
+
+
+@pytest.mark.usefixtures("setup_and_teardown", "log_on_failure")
+class TestSearch:
+    def test_search_valid_product(self):
+        home_page = HomePage(self.driver)
+        search_page = home_page.search_for_product("HP")
+        assert search_page.display_status_of_valid_product()    # 2nd line name has to match with this locator
+
+    def test_search_invalid_product(self):
+        home_page = HomePage(self.driver)
+        search_page = home_page.search_for_product("Honda")
+        expected_txt = "There is no product that matches the search criteria."
+        assert search_page.retrieve_no_product_message().__eq__(expected_txt)
+
+    def test_search_empty_product(self):
+        home_page = HomePage(self.driver)
+        search_page = home_page.search_for_product("")
+        expected_txt = "There is no product that matches the search criteria."
+        assert search_page.retrieve_no_product_message().__eq__(expected_txt)
+
