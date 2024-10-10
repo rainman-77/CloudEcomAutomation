fcfb6bdba07e95b9a4015e49dcabf20e9e85333d
rainman-77 <ashibarcavill99@gmail.com>
2024-10-10 07:27:00 +0530

    updated page object home_page

--- pageobjects/home_page.py
+++ pageobjects/home_page.py
@@ -1,4 +1,5 @@
 from pageobjects.base_page import BasePage
+from pageobjects.login_page import LoginPage
 from pageobjects.search_page import SearchPage
 
 
@@ -9,6 +10,7 @@ class HomePage(BasePage):
     search_box_field_name = {"name": "search"}
     search_button_xpath = {"xpath": "//button[contains(@class, 'btn-default')]"}
     myaccount_dropdown_menu_xpath = {"xpath": "//span[text()='My Account']"}
+    login_option_link_text = {"link_text": "Login"}
 
     def enter_product_into_search_box_field(self, product_name):
         self.type_into_element(product_name, self.search_box_field_name)
@@ -20,6 +22,14 @@ class HomePage(BasePage):
     def click_on_myaccount_dropdown_menu(self):
         self.element_click(self.myaccount_dropdown_menu_xpath)
 
+    def click_on_login_option(self):
+        self.element_click(self.login_option_link_text)
+        return LoginPage(self.driver)
+
     def search_for_product(self, product_name):
         self.enter_product_into_search_box_field(product_name)
         return self.click_on_search_button()
+
+    def navigate_to_login_page(self):
+        self.click_on_myaccount_dropdown_menu()
+        return self.click_on_login_option()
