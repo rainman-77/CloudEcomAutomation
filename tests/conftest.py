c220e2067208995ad720eff5c3632b7b0350e2b0
rainman-77 <ashibarcavill99@gmail.com>
2024-10-15 17:25:00 +0530

    updated for standalone cross browser testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
 from utilities import read_configurations as rc
 
 
@@ -15,7 +18,19 @@ def log_on_failure(request):
 @pytest.fixture()
 def setup_and_teardown(request):
     global driver
-    driver = webdriver.Chrome()
+    browser = rc.read_configuration("basic info", "browser")
+
+    if browser == 'chrome':
+        options = ChromeOptions()
+        driver = webdriver.Chrome(options=options)
+    elif browser == 'firefox':
+        options = FirefoxOptions()
+        driver = webdriver.Firefox(options=options)
+    elif browser == 'edge':
+        options = EdgeOptions()
+        driver = webdriver.Edge(options=options)
+    else:
+        raise ValueError("provide valid browser name in config.ini file")
 
     driver.maximize_window()
     url = rc.read_configuration("basic info", "url")
