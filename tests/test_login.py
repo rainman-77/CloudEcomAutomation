188799034c7dca1fe84995a45f0e2481298c5e11
rainman-77 <ashibarcavill99@gmail.com>
2024-11-24 07:23:00 +0530

    updated test_login.py - due to excel_utils.py update in previous commit

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,13 +7,13 @@ from utilities import excel_utils
 
 @pytest.mark.order(2)  # Set the desired order for this test file
 class TestLogin(BaseTest):
-    @pytest.mark.parametrize("email, psw", excel_utils.get_all_excel_data("login_test"))     # data driven testing
-    def test_login_valid_cred(self, email, psw):
-        logging.info(f"test_login--> test_login_valid_cred started using {email}")
+    @pytest.mark.parametrize("data", excel_utils.get_all_excel_data("login_test"))     # data driven testing
+    def test_login_valid_cred(self, data):
+        logging.info(f"test_login--> test_login_valid_cred started using {data['email']}")
 
         home_page = HomePage(self.driver)
         login_page = home_page.navigate_to_login_page()
-        account_page = login_page.login_with_credentials(email, psw)
+        account_page = login_page.login_with_credentials(data["email"], data["password"])
         assert account_page.is_account_info_text_displayed()
 
         logging.info("test_login--> test_login_valid_cred completed\n")
